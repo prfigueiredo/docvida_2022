e2f0926d604c6b7ad2085eec9fa5b3e74a10b208
Paulo Rodrigo <paulofigueiredo@id.uff.br>
2021-06-18 12:48:25 -0300

    Validação e cadastro de user funcionando.

---
+++ lib/app/controller/signin_controller.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:novo/app/data/model/usuario.dart';
+
+class SigninController extends GetxController{
+
+//Catch data from textfields
+
+TextEditingController controllerNome = TextEditingController();
+TextEditingController controllerEmail = TextEditingController();
+TextEditingController controllerSenha = TextEditingController();
+final _errorMessage = ''.obs;
+
+set errorMensage(value) => _errorMessage.value = value;
+get errorMensage => _errorMessage.value;
+
+
+//Validate data from textfields
+
+validateSigninFields() {
+
+  //getting data from textfields
+  String nome = controllerNome.text;
+  String email = controllerEmail.text;
+  String senha = controllerSenha.text;
+
+  //Validating
+  if (nome.isNotEmpty) {
+    if (email.isNotEmpty && email.contains("@")) {
+      if (senha.isNotEmpty && senha.length > 6) {
+
+        Usuario usuario = Usuario();
+        usuario.nome = nome;
+        usuario.email = email;
+        usuario.senha = senha;
+
+        registerUser(usuario);
+
+      }else{
+        errorMensage = "Digite a sua senha - deve conter mais de 6 caracteres.";
+      }
+    }else{
+      errorMensage = "Preencha o seu E-mail utilizando @";
+    }
+  }else {
+    errorMensage = "Preencha o seu Nome";
+  }
+
+
+
+
+
+}
+
+registerUser(Usuario usuario){
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  auth.createUserWithEmailAndPassword(
+      email: usuario.email,
+      password: usuario.senha
+  ).then((firebaseUser){
+        Get.toNamed('/home');
+  }).catchError((error){
+        print("erro:" + error.toString() );
+        errorMensage = "Ocorreu um erro ao fazer o cadastro.Digite corretamente seus dados.";
+  });
+
+}
+
+}
